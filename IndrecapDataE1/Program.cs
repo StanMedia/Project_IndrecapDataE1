e59045127f74c6b61d84c0f674fac893fb26decc
Slash <wwwroot@mail.ksu.edu.tw>
2023-07-17 09:38:33 +0800

    Init

---
+++ IndrecapDataE1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace IndrecapDataE1
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      System.Console.WriteLine("This project is for sharing internet data.");
+      System.Console.Read();
+    }
+  }
+}
